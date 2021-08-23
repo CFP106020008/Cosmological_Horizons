157b72d6b819a85455a581d0c5d7c96ef472d87c
Lin Yen-Hsing <julius52700@gapp.nthu.edu.tw>
2021-08-24 00:00:16 +0800

    update to double mode

--- main.py
+++ main.py
@@ -8,16 +8,39 @@ from classes import *
 plt.style.use('dark_background')
 fig, ax = plt.subplots(figsize=(6,4))
 
-
 ParaSet1 = []
 
-A = Photon(1e-30, 1.5) 
-ax.plot(A.t/yr2s/1e9, A.a, linestyle='solid', label='$\Omega_m={}$'.format(0.5))
+A = Photon(1e-50, 10) 
 
-ax.set_xlim([0, 16])
-ax.set_ylim([0, 1.5])
-ax.set_xlabel('Cosmic Time (Gyr)')
-ax.set_ylabel('Size of the universe (scale factor)')
-plt.legend()
-fig.savefig('World_Line_Cosmology.png', dpi=300, transparent=True)
-plt.show()
+def LogLog(A=A, fig=fig, ax=ax):
+    ax.loglog(A.t/yr2s/1e9, A.a, linestyle='solid', label='$\Lambda$CDM')
+
+    ax.vlines(9.8,0,2,colors='gray',linestyles='dashed') # Dark Energy Dominated
+    ax.vlines(0.047,0,2,colors='gray',linestyles='dashed') # Dark Energy Dominated
+
+    ax.set_xlim([1e-10, 100])
+    ax.set_ylim([1e-4, 10])
+    ax.set_xlabel('Cosmic Time (Gyr)')
+    ax.set_ylabel('Size of the universe (scale factor)')
+    ax.scatter(13.8, 1, label='Now')
+    plt.legend()
+    fig.savefig('Scale_Factor_loglog.png', dpi=300, transparent=True)
+    ax.cla()
+    #plt.show()
+
+def Normal_Plot(A=A, fig=fig, ax=ax):
+    ax.plot(A.t/yr2s/1e9, A.a, linestyle='solid', label='$\Lambda$CDM')
+    #ax.vlines(9.8,0,2,colors='gray',linestyles='dashed') # Dark Energy Dominated
+    #ax.vlines(0.047,0,2,colors='gray',linestyles='dashed') # Dark Energy Dominated
+    ax.set_xlim([0, 20])
+    ax.set_ylim([0, 2])
+    ax.set_xlabel('Cosmic Time (Gyr)')
+    ax.set_ylabel('Size of the universe (scale factor)')
+    ax.scatter(13.8, 1, label='Now')
+    plt.legend()
+    fig.savefig('Scale_Factor.png', dpi=300, transparent=True)
+    ax.cla()
+    #plt.show()
+
+Normal_Plot()
+LogLog()
